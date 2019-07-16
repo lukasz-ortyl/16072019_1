25e9ce90355235e30858ab9c06445e8254eff44a
lukasz <lukasz.ortyl@gmail.com>
2019-07-16 15:38:47 +0200

    C_3 po wszystkich klasach

--- Company.py
+++ Company.py
@@ -33,27 +33,73 @@ class Employee:
         raise NotImplementedError()
     # jesli program nie znajdzie monty_salary w klasie podrzednej to odwoła sie wyzej a nie chcemy
 
-class SalariedEmployee(Employee)
+class SalariedEmployee(Employee):
     def __init__(self, name, age, task_list, weekly_salary):
         super().__init__(name, age, task_list)
         self.weekly_salary = weekly_salary
 
-class HourlyEmployee(Employee)
+    def describe(self):
+        return "{} I earn {} weekly".format(super().describe(), self.weekly_salary)
+
+    @property
+    def monty_salary(self):
+        return self.monty_salary * 4
+
+class HourlyEmployee(Employee):
     def __init__(self, name, age, task_list, week_work_hrs, hour_salary):
         super().__init__(name, age, task_list)
         self.week_work_hrs = week_work_hrs
         self.hour_salary = hour_salary
 
+    def describe(self):
+        return "{} I work {} hours weekly".format(super().describe(), self.week_work_hrs)
+
+    @property
+    def monty_salary(self):
+        return self.hour_salary*self.week_work_hrs * 4
+
+class Company:
+    def __init__(self, name, emp_list, task_list):
+        self.name = name
+        self.emp_list = emp_list
+        self.task_list = task_list
+
+    def add_employees(self, employee):
+        self.emp_list.append(employee)
+
+    def add_task(self, task):
+        self.task_list.append(task)
+
+    def dist_task(self):
+        for task in self.task_list:
+            self.emp_list[0].add_task(task)
+
+
+
+
 
 
 if __name__ == '__main__':
     zad_1 = Task("Write Art", 140)
-    zad_2 = Task("MSc review", 3)
-    zad_3 = Task("TBZ classes", 2)
-    emp_1_list = [zad_1, zad_3]
-    emp_2_list = [zad_1, zad_2]
-    emp_1 = Employee("Łukasz", 42, emp_1_list)
-    emp_2 = Employee("Tomasz", 39, emp_2_list)
-
+    zad_2 = Task("Write Art", 140)
+    zad_3 = Task("write Art", 140)
+    zad_4 = Task("MSc review", 3)
+    zad_5 = Task("TBZ classes", 2)
+    zad_6 = Task("PP classes", 5)
+    emp_1_list = []
+    emp_2_list = []
+    emp_3_list = []
+    emp_1 = SalariedEmployee("Łukasz", 42, emp_1_list)
+    emp_2 = HourlyEmployee("Tomasz", 39, emp_2_list)
+    #emp_3 = Employee("Przemo", 34, emp_3_list)
+    my_emp_list = []
+    my_task_list = []
+    my_company = Company("AGH", my_emp_list, my_task_list)
+    my_company.add_employees(emp_1)
+    my_company.add_employees(emp_2)
+    my_company.add_task(zad_1)
+    my_company.add_task(zad_2)
+    my_company.add_task(zad_3)
+    my_company.add_task(zad_4)
     emp_1.work()
     print(emp_1.points_sum)
